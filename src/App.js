70978e732bc7e383334b7da4c1130da89f1d62ea
BN <rln71@att.net>
2020-02-03 12:13:32 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
         <p>
         Hello World!        
         </p>
-      </header>
+      
     </div>
   );
 }
